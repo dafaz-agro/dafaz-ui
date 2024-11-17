dcebe8a34bec34bf9550f7b75c6538ec879c0b5e
asammarco <asammarco@hotmail.com>
2024-11-17 14:10:36 -0300

    fix: Failed to fetch dynamically imported module

--- packages/docs/storybook-static/assets/Configure-Dqsef_xZ.js
+++
@@ -1,3 +0,0 @@
-import{j as t}from"./jsx-runtime-j_jdvEMj.js";import{useMDXComponents as r}from"./index-BSj771as.js";import{ae as i}from"./index-u1K3z0g2.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BeWqwfQV.js";import"../sb-preview/runtime.js";import"./index-Blc9requ.js";import"./index-DolzVqEf.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",p:"p",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Dafaz"}),`
-`,t.jsx(o.h1,{id:"dafaz-ui",children:"Dafaz UI"}),`
-`,t.jsx(o.p,{children:"Este é o Design System da Dafaz."})]})}function D(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{D as default};
